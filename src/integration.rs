8a962df0e54ca2fdeef3e8d486c8e1ab280106d9
NotAPenguin <michiel.vandeginste@gmail.com>
2023-06-12 12:54:11 +0200

    fix: set correct blend options

--- src/integration.rs
+++ src/integration.rs
@@ -75,7 +75,7 @@ impl<A: Allocator + 'static> Integration<A> {
             .blend_additive_unmasked(
                 vk::BlendFactor::SRC_ALPHA,
                 vk::BlendFactor::ONE_MINUS_SRC_ALPHA,
-                vk::BlendFactor::ZERO,
+                vk::BlendFactor::ONE_MINUS_DST_ALPHA,
                 vk::BlendFactor::ONE,
             )
             .build();
